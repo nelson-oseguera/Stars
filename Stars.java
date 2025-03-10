9f60a6a2dff3aff6dbb17157d185868516687f87
Nelson Oseguera <nelsonoseguera@outlook.com>
2025-03-10 00:37:18 -0400

    Add files via upload

---
+++ Stars.java
@@ -0,0 +1,34 @@
+package stars;
+
+//Nelson Oseguera [os1148644@email.ccbcmd.edu: ]
+/**
+		* Overview: Write a program that prints out a diamond shape made out of stars
+		* Input: None
+		* Output: Text Display * Variables: None
+		* Plan:
+		* 1. Print out multiple lines of stars
+		* 2. Program will show a diamond shape
+		*
+		* @version: 1.0 August 30, 2023
+		* @author: Nelson / Oseguera
+*/
+
+public class Stars {
+	 
+	 public static void main(String[] args) {
+		 
+		//print multiple lines of stars to create diamond shape
+		 
+		           System.out.println("     *");
+		           System.out.println("    ***");
+		           System.out.println("   *****");
+		           System.out.println("  *******");
+		           System.out.println(" *********");
+		           System.out.println("  *******");
+		           System.out.println("   *****");
+		           System.out.println("    ***");
+		           System.out.println("     *");
+		 
+		     }
+
+}
